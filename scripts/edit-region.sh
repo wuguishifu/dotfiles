befde1efc2cc930f686c9bb31f8537537d1d774d
Bo Bramer <bramer.bo@gmail.com>
2026-06-19 11:01:36 -0700

    this is a 2 space indent household

--- scripts/edit-region.sh
+++ scripts/edit-region.sh
@@ -1,59 +1,59 @@
 #!/bin/sh
 
 update_managed_region() {
-    file="$1"
-    comment="$2"
+  file="$1"
+  comment="$2"
 
-    content_file=$(mktemp)
-    cat > "$content_file"
+  content_file=$(mktemp)
+  cat > "$content_file"
 
-    start="$comment MANAGED BY wuguishifu/dotfiles START"
-    end="$comment MANAGED BY wuguishifu/dotfiles END"
+  start="$comment MANAGED BY wuguishifu/dotfiles START"
+  end="$comment MANAGED BY wuguishifu/dotfiles END"
 
-    tmp=$(mktemp)
+  tmp=$(mktemp)
 
-    awk -v start="$start" \
-        -v end="$end" \
-        -v content_file="$content_file" '
-    BEGIN {
-        found = 0
+  awk -v start="$start" \
+    -v end="$end" \
+    -v content_file="$content_file" '
+  BEGIN {
+    found = 0
 
-        while ((getline line < content_file) > 0) {
-            content[++n] = line
-        }
-        close(content_file)
+    while ((getline line < content_file) > 0) {
+      content[++n] = line
     }
+    close(content_file)
+  }
 
-    $0 == start {
-        print
-        for (i = 1; i <= n; i++) {
-            print content[i]
-        }
-        found = 1
-        skip = 1
-        next
+  $0 == start {
+    print
+    for (i = 1; i <= n; i++) {
+      print content[i]
     }
+    found = 1
+    skip = 1
+    next
+  }
 
-    $0 == end {
-        skip = 0
-        print
-        next
+  $0 == end {
+    skip = 0
+    print
+    next
+  }
+
+  !skip { print }
+
+  END {
+    if (!found) {
+      print ""
+      print start
+      for (i = 1; i <= n; i++) {
+        print content[i]
+      }
+      print end
     }
+  }
+  ' "$file" > "$tmp" &&
+    mv "$tmp" "$file"
 
-    !skip { print }
-
-    END {
-        if (!found) {
-            print ""
-            print start
-            for (i = 1; i <= n; i++) {
-                print content[i]
-            }
-            print end
-        }
-    }
-    ' "$file" > "$tmp" &&
-        mv "$tmp" "$file"
-
-    rm -f "$content_file"
+  rm -f "$content_file"
 }
